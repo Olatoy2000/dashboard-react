8458bbe79d0516373cb3a1c8e072f94ac88d5498
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-07-24 08:47:56 +0100

    designing the layout for the dahshboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
 function App() {
 
-  return (
-    <div>Hello World</div>
-  )
+
+
+
+  const Layout = () => {
+    <div className="main"></div>
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "users",
+      element: <Users />,
+    },
+    {
+      path: "products",
+      element: <Products />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
