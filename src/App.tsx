6c32741fe327136736ac69a12b3b5c0742769695
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-07-24 09:22:44 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+
 import Home from "./pages/home/Home";
 import Products from "./pages/products/Products";
 import Users from "./pages/users/Users";
@@ -7,35 +8,47 @@ import Users from "./pages/users/Users";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
+ Outlet,
 } from "react-router-dom";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const Layout = () => {
     <div className="main">
       <Navbar />
-      <div>
-        <div className="menuContainer"></div>
-        <div className="contactContainer"></div>
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contactContainer">
+          <Outlet />
+        </div>
       </div>
       <Footer />
     </div>;
   };
 
+
   const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
-    },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/users",
+        element: <Users />
+      },
+      {
+        path: "/products",
+        element: <Products />
+      },
+    ]
+  },
+  
   ]);
   return <RouterProvider router={router} />;
 }
