91d733bb6cfdcd79300bc7847db82008fc527202
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-07-24 15:02:58 +0100

    fixing bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-
 import Home from "./pages/home/Home";
 import Products from "./pages/products/Products";
 import Users from "./pages/users/Users";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
- Outlet,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Menu from "./components/menu/Menu";
 
 function App() {
-  const Layout = () => {
+  const Layout = () => (
     <div className="main">
       <Navbar />
       <div className="container">
@@ -25,30 +20,27 @@ function App() {
         </div>
       </div>
       <Footer />
-    </div>;
-  };
-
-
+    </div>
+  );
   const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/users",
-        element: <Users />
-      },
-      {
-        path: "/products",
-        element: <Products />
-      },
-    ]
-  },
-  
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
   ]);
   return <RouterProvider router={router} />;
 }
