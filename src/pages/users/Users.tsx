d7331de90a6eca09a92a15192f4cd5d614dbfb1d
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-07-26 11:40:19 +0100

    user ui design implementation beginning

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,15 @@
-import "./users.scss"
-
+import DataTable from "../../components/dataTable/DataTable";
+import "./users.scss";
 
 const Users = () => {
-    return (
-        <div className="users">Users</div>
-    )
-}
-export default Users
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+        <DataTable />
+      </div>
+    </div>
+  );
+};
+export default Users;
